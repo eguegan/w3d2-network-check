bbc0e2a643b43e97fb6547911988d4b225847fda
eguegan <e.guegan@margulls.com>
2016-04-12 16:36:38 -0400

    Add checkInternet method

--- app/src/main/java/com/example/admin/w3d2networkcheck/MainActivity.java
+++ app/src/main/java/com/example/admin/w3d2networkcheck/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.admin.w3d2networkcheck;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkInternet(View view) {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                Log.d(TAG, "checkInternet: " + "Connected to WIFI");
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                Log.d(TAG, "checkInternet: " + "Connected to Mobile data");
+            }
+        } else {
+            Log.d(TAG, "checkInternet: " + "Not connected");
+        }
     }
 }
